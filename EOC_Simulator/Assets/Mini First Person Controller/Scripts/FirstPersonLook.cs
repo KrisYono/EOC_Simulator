e0f639f83636148f7a30e2e7a4a046f4007643f0
Oscar Rützou <oscargamer999@gmail.com>
2025-01-20 10:10:47 -0800

    Basic Test Movement

---
+++ EOC_Simulator/Assets/Mini First Person Controller/Scripts/FirstPersonLook.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class FirstPersonLook : MonoBehaviour
+{
+    [SerializeField]
+    Transform character;
+    public float sensitivity = 2;
+    public float smoothing = 1.5f;
+
+    Vector2 velocity;
+    Vector2 frameVelocity;
+
+    private FirstPersonMovement _firstPersonMovement;
+    void Awake()
+    {
+        // Get the character from the FirstPersonMovement in parents.
+        _firstPersonMovement = GetComponentInParent<FirstPersonMovement>();
+        character = _firstPersonMovement.transform;
+    }
+
+    void Update()
+    {
+        if (_firstPersonMovement.IsTopDownView)
+            transform.localRotation = Quaternion.Euler(45, 0, 0);
+        else         
+            transform.localRotation = Quaternion.AngleAxis(-velocity.y, Vector3.right);
+
+        character.localRotation = Quaternion.AngleAxis(velocity.x, Vector3.up);
+        
+        if (!Input.GetMouseButton(1)) return;
+        
+        // Get smooth velocity.
+        Vector2 mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        Vector2 rawFrameVelocity = Vector2.Scale(mouseDelta, Vector2.one * sensitivity);
+        frameVelocity = Vector2.Lerp(frameVelocity, rawFrameVelocity, 1 / smoothing);
+        velocity += frameVelocity;
+        velocity.y = Mathf.Clamp(velocity.y, -90, 90);
+
+        // Rotate camera up-down and controller left-right from velocity.
+        if (_firstPersonMovement.IsTopDownView)
+            transform.localRotation = Quaternion.Euler(45, 0, 0);
+        else         
+            transform.localRotation = Quaternion.AngleAxis(-velocity.y, Vector3.right);
+
+        character.localRotation = Quaternion.AngleAxis(velocity.x, Vector3.up);
+    }
+}
+
---
+++ EOC_Simulator/Assets/Mini First Person Controller/Scripts/FirstPersonMovement.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonMovement : MonoBehaviour
+{
+
+    [Header("Running")]
+    public float speed = 5;
+    public float runSpeed = 9;
+    public bool canRun = true;
+    public bool IsRunning { get; private set; }
+    public KeyCode runningKey = KeyCode.LeftShift;
+    
+    private Rigidbody rigidbody;
+    private Vector3 hitPoint;
+    private bool hitWalkablePosition = false;
+
+    [HideInInspector] public bool IsWASDMovement = true;
+    [HideInInspector] public bool IsTopDownView = false;
+        
+    [SerializeField] private Camera topDownCamera;
+    [SerializeField] private Camera fpsCamera;
+    [SerializeField] private LayerMask walkableLayer;
+    
+    [SerializeField] private float sphereSize = 0.2f;
+    private bool canWalkOnHitPoint;
+
+    void Awake()
+    {
+        // Get the rigidbody on this.
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    public void ChangeMovementType(bool isWADS)
+    {
+        IsWASDMovement = isWADS;
+    }
+
+    public void ChangeView(bool isTopDown)
+    {
+        IsTopDownView = isTopDown;
+    }
+    
+    void FixedUpdate()
+    {
+        if (IsWASDMovement) WASDMovement();
+        else TeleportPlayer();
+    }
+    void TeleportPlayer()
+    {
+        Camera cam = fpsCamera;
+        if (IsTopDownView) cam = topDownCamera;
+        
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        hitWalkablePosition = false;
+            
+        if (Physics.Raycast(ray, out RaycastHit hit, 100, walkableLayer))
+        {
+            var objectHit = hit.transform.gameObject;
+            hitPoint = hit.point;
+
+            var collides = Physics.OverlapSphere(hitPoint, sphereSize);
+            if (collides != null && collides.Length > 0)
+            {
+                foreach (var collide in collides)
+                {
+                    Debug.Log($"Amount: {collides.Length} - {collide.name}");
+                    if (!collide.CompareTag("WalkableArea"))
+                    {
+                        canWalkOnHitPoint = false;
+                        return;
+                    }
+                }
+            }
+            
+            canWalkOnHitPoint = true;
+            
+            if (!Input.GetMouseButtonDown(0)) return;
+            
+            Debug.Log($"Hit {objectHit.name}");
+            if (objectHit != null)
+            {
+                // Debug.Log(objectHit.name);
+                Debug.Log(objectHit.tag);
+                hitWalkablePosition = true;
+                // Make another check
+                transform.position = hitPoint;
+            }
+        }
+    }
+
+    
+    void WASDMovement()
+    {
+        // Update IsRunning from input.
+        IsRunning = canRun && Input.GetKey(runningKey);
+
+        // Get targetMovingSpeed.
+        float targetMovingSpeed = IsRunning ? runSpeed : speed;
+
+        // Get targetVelocity from input.
+        Vector2 targetVelocity =new Vector2( Input.GetAxis("Horizontal") * targetMovingSpeed, Input.GetAxis("Vertical") * targetMovingSpeed);
+
+        // Apply movement.
+        rigidbody.linearVelocity = transform.rotation * new Vector3(targetVelocity.x, rigidbody.linearVelocity.y, targetVelocity.y);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (canWalkOnHitPoint) Gizmos.color = Color.green;
+        else Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(hitPoint, sphereSize);
+        
+    }
+}
